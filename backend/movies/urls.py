5b619721871c963bf3c00b26b7eb67b14460f55c
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2021-03-28 04:01:39 -0400

    chore(api): serve API under api/ path
    So as not to interfere with serving of frontend app when site is served by nginx

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
 from . import views
 
 router = SimpleRouter(trailing_slash=False)
-
 router.register('movies', views.MovieViewSet, basename='movies')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-] + router.urls
+]
